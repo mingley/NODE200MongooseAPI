896dfedbab55f78acb1b20199291546bbaacf53f
mikeingley <michael.ingley@gmail.com>
2019-09-05 18:06:05 -0700

    11

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
+mongoose.connect('MONGODB_URIgit ', { useMongoClient: true });
 mongoose.Promise = Promise;
 mongoose.set('useFindAndModify', false);
 
